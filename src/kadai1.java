450547a1cae27bb06bae9a18bd6f1ed9ba729454
AdachiHiroki <241039@std.hi-joho.ac.jp>
2025-05-16 11:37:12 +0900

    課題1

---
+++ src/kadai1.java
@@ -0,0 +1,9 @@
+public class kadai1 {
+    public static void main(String[] args){
+        System.out.print("こんにちは。");
+        System.out.println("\n" + "今日の天気は" + " \t" + "晴れです。");
+        System.out.println("今日は一日" + "\n" + "洗濯日和です");
+        System.out.println("干しても大丈夫そうです。");
+        System.out.println("文字は" + "”" + "囲みましょう");
+    }
+}
